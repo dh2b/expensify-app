539b1c681ba66e0bb12b0f46f0d395beb1cc467c
Darcio Bertoco <darciohbb@gmail.com>
2019-06-20 14:12:46 -0300

    Allow select account on login

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,6 +15,10 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
+googleAuthProvider.setCustomParameters({
+	prompt: 'select_account'
+});
+
 export { firebase, googleAuthProvider, database as default };
 
 // //child_removed
